4de0662534f47f53873322fe8325408a7dd85718
joncody <jonjdcody@gmail.com>
2018-05-08 18:44:46 -0700

    .

--- js/stripal.js
+++ js/stripal.js
@@ -146,8 +146,10 @@
 
     stripal.empty = function () {
         stripal.each(function (item) {
+            item.quiet(true);
             item.remove();
         });
+        stripal.emit("empty");
     };
 
     stripal.stripeKey = function (key) {
